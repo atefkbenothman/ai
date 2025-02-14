4ce0d1b615bbb0179845be501c4d68caebd5cb53
Atef Kai Benothman <atefkbenothman@gmail.com>
2025-02-13 18:16:24 -0800

    chore(prod): get ready and clean up for prod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,8 @@
 
 import { PageContent } from "@/components/page-content"
 import { Separator } from "@/components/ui/separator"
-// import { createClerkSupabaseClientSsr } from "@/server/supabase/server"
 
 export default async function Home() {
-  // const client = await createClerkSupabaseClientSsr()
-  // const { data, error } = await client.from("providers").select()
-  // if (error) {
-  //   console.error(error)
-  // }
-  // const providers = data
-
   return (
     <PageContent>
       <div className="flex items-center justify-between py-1">
@@ -22,9 +14,6 @@ export default async function Home() {
         <div className="mr-2 flex flex-1 items-center justify-end"></div>
       </div>
       <Separator className="h-[0.09rem]" />
-      {/* {providers?.map((provider: any, idx: number) => (
-        <p key={idx}>{provider.id} - {provider.name}</p>
-      ))} */}
     </PageContent>
   )
 }
--- src/components/nav/app-sidebar.tsx
+++ src/components/nav/app-sidebar.tsx
@@ -12,7 +12,6 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
-import { SignedIn, SignedOut, useUser } from "@clerk/nextjs"
 import { Home, MessageCircle, Settings, User } from "lucide-react"
 
 const items = [
@@ -35,7 +34,6 @@ const items = [
 
 export function AppSidebar() {
   const pathname = usePathname()
-  const { user } = useUser()
 
   return (
     <Sidebar collapsible="icon">
@@ -67,34 +65,18 @@ export function AppSidebar() {
       <SidebarFooter className="">
         <SidebarGroupContent>
           <SidebarMenu className="gap-2">
-            <SignedOut>
-              <SidebarMenuItem>
-                <SidebarMenuButton
-                  asChild
-                  className="items-center gap-4"
-                  isActive={pathname === "/login"}
-                >
-                  <Link href="/login" className="text-lg font-medium">
-                    <User strokeWidth={2.3} />
-                    <span>Log In</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SignedOut>
-            <SignedIn>
-              <SidebarMenuItem>
-                <SidebarMenuButton
-                  asChild
-                  className="items-center gap-4"
-                  isActive={pathname === "/login"}
-                >
-                  <Link href="/login" className="text-lg font-medium">
-                    <User strokeWidth={2.3} />
-                    <span>{user?.fullName}</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SignedIn>
+            <SidebarMenuItem>
+              <SidebarMenuButton
+                asChild
+                className="items-center gap-4"
+                isActive={pathname === "/login"}
+              >
+                <Link href="/login" className="text-lg font-medium">
+                  <User strokeWidth={2.3} />
+                  <span>Log In</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
           </SidebarMenu>
         </SidebarGroupContent>
       </SidebarFooter>
